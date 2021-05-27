37fff81bae806763924cb8892615b57ae01237d1
易继勇 <yijiyong100@163.com>
2021-05-27 16:48:53 +0800

    增加评论 使用github vssue

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -12,26 +12,24 @@ module.exports = {
     ['meta', { name: 'author', content: 'landry -- seo 选项' }],
     ['meta', { name: 'keywords', content: '后端 java 架构 面试 -- seo 选项' }]
   ],
-  plugins: [
-    [
-      '@vuepress/last-updated',
-      {
-        transformer: (timestamp) => {
-          // 不要忘了安装 moment
-          return moment(timestamp).format("LLLL")
-        }
+  plugins: {
+    '@vuepress/last-updated': {
+      transformer: (timestamp) => {
+        // 不要忘了安装 moment
+        return moment(timestamp).format("LLLL")
       }
-    ],
-    [
-      '@vuepress/pwa', {
-        serviceWorker: true,
-        updatePopup: {
-          message: "发现新内容",
-          buttonText: "刷新"
-        }
-      }
-    ]
-  ],
+    },
+    '@vssue/vuepress-plugin-vssue': {
+      // 设置 `platform` 而不是 `api`
+      platform: 'github-v4',
+      // 其他的 Vssue 配置
+      owner: 'yijiyong100',
+      repo: 'aibeadocs',
+      clientId: '38fc681f3b83868f76c6',
+      clientSecret: '9f67078aecce20a2327f0da9f9e0fb5bbe4e1e87',
+      autoCreateIssue: true
+    }
+  },
   themeConfig: {
     logo: '/assets/img/logo.png',
     sidebar: 'auto',

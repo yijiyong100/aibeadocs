7e8d6a2d52e707fa6d209728289d3360ce4fb17e
易继勇 <yijiyong100@163.com>
2021-06-10 10:41:56 +0800

    增加 自定义主题侧边  二维码

--- docs/.vuepress/theme/layouts/Layout.vue
+++ docs/.vuepress/theme/layouts/Layout.vue
@@ -155,20 +155,6 @@ export default {
   },
 
   methods: {
-
-    qrCode (url) {
-      document.getElementById('qrcode').innerHTML = '';
-      let qrcode = new QRCode('qrcode', {
-        width: 150, //图像宽度
-        height: 150, //图像高度
-        colorDark: "#000000", //前景色
-        colorLight: "#ffffff", //背景色
-        typeNumber: 4,
-        correctLevel: QRCode.CorrectLevel.H //容错级别 容错级别有：（1）QRCode.CorrectLevel.L （2）QRCode.CorrectLevel.M （3）QRCode.CorrectLevel.Q （4）QRCode.CorrectLevel.H
-      })
-      qrcode.clear() //清除二维码 
-      qrcode.makeCode(url) //生成另一个新的二维码
-    },
     // 全屏事件
     handleFullScreen () {
       var element = document.documentElement;
@@ -211,6 +197,20 @@ export default {
       this.curUrlPath = window.location.pathname
       this.curAllUrlPath = window.location.href
       this.qrCode(this.curAllUrlPath)
+    },
+    qrCode (url) {
+      var qrCodeDiv = document.getElementById('qrcode');
+      qrCodeDiv.innerHTML = '';
+      let qrcode = new QRCode('qrcode', {
+        width: 150, //图像宽度
+        height: 150, //图像高度
+        colorDark: "#000000", //前景色
+        colorLight: "#ffffff", //背景色
+        typeNumber: 4,
+        correctLevel: QRCode.CorrectLevel.H //容错级别 容错级别有：（1）QRCode.CorrectLevel.L （2）QRCode.CorrectLevel.M （3）QRCode.CorrectLevel.Q （4）QRCode.CorrectLevel.H
+      })
+      qrcode.clear() //清除二维码 
+      qrcode.makeCode(url) //生成另一个新的二维码
     },
     initGetPageNextLast () {
       var prevUpdated = 0;

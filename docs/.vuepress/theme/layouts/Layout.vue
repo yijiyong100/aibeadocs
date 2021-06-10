4992986fd37340a355cb322467b923bb983dce98
易继勇 <yijiyong100@163.com>
2021-06-10 10:31:55 +0800

    增加 自定义主题侧边  二维码

--- docs/.vuepress/theme/layouts/Layout.vue
+++ docs/.vuepress/theme/layouts/Layout.vue
@@ -37,8 +37,12 @@
         <span>{{this.isLeftSiderOpen}}</span>
       </div>
 
-      <span v-if="this.pagePrevUrl.length > 0"><a :href="this.pagePrevUrl">上一篇</a></span>
-      <span v-if="this.pageNextUrl.length > 0"><a :href="this.pageNextUrl">下一篇</a></span>
+      <span v-if="this.pagePrevUrl.length > 0"><a :href="this.pagePrevUrl" class="prev">上一篇</a></span>
+      <span v-if="this.pageNextUrl.length > 0"><a :href="this.pageNextUrl" class="next">下一篇</a></span>
+
+      <div>当前页面地址：{{this.curAllUrlPath}}</div>
+      <div>二维码</div>
+      <div id="qrcode" ref="qrcode"></div>
 
       <ul>
         <li v-for="item in $page.headers" v-bind:key="item.slug">
@@ -60,6 +64,8 @@ import Navbar from '@theme/components/Navbar.vue'
 import Page from '@theme/components/Page.vue'
 import Sidebar from '@theme/components/Sidebar.vue'
 import { resolveSidebarItems } from '../util'
+import QRCode from 'qrcodejs2'
+
 
 export default {
   name: 'Layout',
@@ -75,7 +81,10 @@ export default {
     return {
       isSidebarOpen: false,
       curIndexSlug: "",
+      // 相对路径
       curUrlPath: "",
+      // 全路径 
+      curAllUrlPath: "",
       isLeftSiderOpen: true,
       isFullscreen: false,
       pageNextUrl: "",
@@ -146,6 +155,20 @@ export default {
   },
 
   methods: {
+
+    qrCode (url) {
+      document.getElementById('qrcode').innerHTML = '';
+      let qrcode = new QRCode('qrcode', {
+        width: 150, //图像宽度
+        height: 150, //图像高度
+        colorDark: "#000000", //前景色
+        colorLight: "#ffffff", //背景色
+        typeNumber: 4,
+        correctLevel: QRCode.CorrectLevel.H //容错级别 容错级别有：（1）QRCode.CorrectLevel.L （2）QRCode.CorrectLevel.M （3）QRCode.CorrectLevel.Q （4）QRCode.CorrectLevel.H
+      })
+      qrcode.clear() //清除二维码 
+      qrcode.makeCode(url) //生成另一个新的二维码
+    },
     // 全屏事件
     handleFullScreen () {
       var element = document.documentElement;
@@ -186,6 +209,8 @@ export default {
     },
     initPath () {
       this.curUrlPath = window.location.pathname
+      this.curAllUrlPath = window.location.href
+      this.qrCode(this.curAllUrlPath)
     },
     initGetPageNextLast () {
       var prevUpdated = 0;

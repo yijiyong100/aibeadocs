509c8e5e555b39347d4e4b5958508e6861ee1d0a
易继勇 <yijiyong100@163.com>
2021-07-06 15:53:14 +0800

    增加 优化访问 后端+前端优化2 fixed #35

--- docs/.vuepress/enhanceApp.js
+++ docs/.vuepress/enhanceApp.js
@@ -1,12 +1,12 @@
 import Element from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css';
-import TestCommP from './theme/components/TestCommP.vue'
-import ArticleVisit from './theme/components/ArticleVisit.vue'
+// import TestCommP from './theme/components/TestCommP.vue'
+// import ArticleVisit from './theme/components/ArticleVisit.vue'
 
 
 
 export default ({ Vue, options, router, siteData }) => {
   Vue.use(Element);
-  Vue.component('TestCommP', TestCommP)
-  Vue.component('ArticleVisit', ArticleVisit)
+  // Vue.component('TestCommP', TestCommP)
+  // Vue.component('ArticleVisit', ArticleVisit)
 };
